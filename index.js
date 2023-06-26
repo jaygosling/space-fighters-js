5e30473f2f64107cdc03b4d5a623536ed48ce778
jaygosling <javijimenezclemente@gmail.com>
2023-06-26 10:45:34 +0200

    Added managing missed shots and improved bullet movement. Adde Level Clear screen.

--- index.js
+++ index.js
@@ -3,23 +3,28 @@ var shipPos = 95;
 var invadersPos = [12, 13, 14, 15, 16, 17, 22, 23, 24, 25, 26, 27, 32, 33, 34, 35, 36, 37]
 var bulletPos = 0;
 var score = 0;
+var bulletShot = false;
 
-for (let i = 0; i < 10; i++) {
-    var row = document.createElement('div');
-    row.classList.add("d-flex");
+
+function drawBoard() {
     for (let i = 0; i < 10; i++) {
-        var squares = document.createElement('div');
-        squares.style.height = "50px";
-        squares.style.width = "50px";
-        squares.classList.add("bg-success");
-        squares.classList.add("squares");
-        squares.classList.add("d-flex");
-        squares.classList.add("justify-content-center");
-        squares.classList.add("align-content-middle");
-        row.appendChild(squares);
+        var row = document.createElement('div');
+        row.classList.add("d-flex");
+        for (let i = 0; i < 10; i++) {
+            var squares = document.createElement('div');
+            squares.style.height = "50px";
+            squares.style.width = "50px";
+            squares.classList.add("bg-success");
+            squares.classList.add("squares");
+            squares.classList.add("d-flex");
+            squares.classList.add("justify-content-center");
+            squares.classList.add("align-content-middle");
+            row.appendChild(squares);
+        }
+        myApp.appendChild(row);
     }
-    myApp.appendChild(row);
 }
+drawBoard();
 
 var board = document.getElementsByClassName("squares");
 var spaceship = document.createElement("img")
@@ -34,8 +39,8 @@ scoreboard.innerHTML = score.toString().padStart(5, "0")
 scoreboard.style.position = "relative"
 scoreboard.style.left = "40px"
 scoreboard.style.top = "15px"
-board[0].appendChild(scoreboard)
 
+board[0].appendChild(scoreboard)
 board[shipPos].appendChild(spaceship)
 
 function drawInvaders() {
@@ -62,8 +67,29 @@ function checkInvader() {
         return check
     }
 }
+var levelClr = document.createElement("div")
+levelClr.innerHTML = "LEVEL<br>CLEAR"
+levelClr.classList.add("level-clear")
+levelClr.classList.add("d-none")
+levelClr.style.position = "relative"
+levelClr.style.left = "150px"
+levelClr.style.bottom = "350px"
+myApp.appendChild(levelClr)
 
-function shoot() {
+
+
+function checkWin() {
+    let allClear = 0;
+    for (let x of invadersPos) {
+        allClear += x;
+    }
+    if (!allClear){
+        levelClr.classList.remove("d-none")
+        console.log("Ganaste")
+    }
+}
+
+function shoot(myI) {
     console.log("entra en shoot")
     var bullet = document.getElementById('bullet');
     board[bulletPos].removeChild(bullet)
@@ -74,24 +100,35 @@ function shoot() {
     if (hit !== false) {
         console.log("HIT")
         board[bulletPos].removeChild(bullet)
-        console.log(invader[hit])
+        console.log(invaders[hit])
         invaders[hit].classList.add('d-none')
         score+=100
         scoreboard.innerHTML = score.toString().padStart(5, "0")
         invadersPos[hit] = 0
         bulletPos = 0
+        bulletShot = false
+        clearInterval(myI)
+        checkWin()
     } else {
         console.log("NOT HIT")
+            if(bulletPos < 10 && bulletShot == true) {
+        clearInterval(myI)
+        board[bulletPos].removeChild(bullet)
+        bulletShot = false
+        bulletPos = 0
+        return false
     }
+    }
+
 }
 
-/* DEBUG BUTTON
+// DEBUG BUTTON
 
 var button = document.createElement("button")
 button.addEventListener('click', debugBtn)
 myApp.appendChild(button)
 
-*/
+
 
 function debugBtn() {
     shoot();
@@ -112,8 +149,8 @@ function keyHandle(event) {
             board[shipPos].appendChild(spaceship)
         }
     }
-    if (event.key == " ") {
-        console.log("Empieza el Espacio")
+    if (event.key == " " && bulletShot == false) {
+        bulletShot = true
         var bullet = document.createElement("img")
         bullet.src = "bala.png"
         bullet.style.height = "25px"
@@ -121,12 +158,7 @@ function keyHandle(event) {
         bullet.id = 'bullet'
         bulletPos = shipPos-10
         board[bulletPos].appendChild(bullet)
-        while(bulletPos > 0){
-            console.log("Entra en el while")
-            //setTimeout(shoot, 1000);
-            //clearTimeout();
-            shoot()
-        }
+        let  myInterval = setInterval(()=> shoot(myInterval), 50);
     }
 }
 
